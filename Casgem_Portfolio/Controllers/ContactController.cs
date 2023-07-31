f59a4e4842966355e26620ff643955453bc1b513
sahin <sahin@DESKTOP-1SCT98J>
2023-07-31 13:33:32 +0300

    Add project files.

---
+++ Casgem_Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Casgem_Portfolio.Models.Entities;
+
+namespace Casgem_Portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        CasgemPortfolioEntities db = new CasgemPortfolioEntities();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index(TblMessage p)
+        {
+            db.TblMessage.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Portfolio");
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var values = db.TblContact.ToList();
+            return View(values);
+        }
+    }
+}
